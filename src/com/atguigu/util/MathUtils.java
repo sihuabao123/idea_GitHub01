c7a8049595aaaf66d0ca4fec33dccf3c8a8b8838
司华宝 <2101132175@qq.com>
2021-02-02 22:57:36 +0800

    更改masterMathUtils类中输出1111master

--- src/com/atguigu/util/MathUtils.java
+++ src/com/atguigu/util/MathUtils.java
@@ -2,6 +2,6 @@ package com.atguigu.util;
 
 public class MathUtils {
     public static void main(String[] args) {
-        System.out.println("11111");
+        System.out.println("11111master");
     }
 }
