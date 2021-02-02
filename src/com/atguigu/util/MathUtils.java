fe80d5ddb62a122e032004d772d868abd75ef23f
sihuabao123 <78328077+sihuabao123@users.noreply.github.com>
2021-02-02 23:20:01 +0800

    再次更改用于idea的pull
    再次更改用于idea的pull

--- src/com/atguigu/util/MathUtils.java
+++ src/com/atguigu/util/MathUtils.java
@@ -6,5 +6,6 @@ public class MathUtils {
         System.out.println("11111dev");
         System.out.println("GitHub1111");
         System.out.println("clone完成之后更改");
+        System.out.println("再次更改用于idea的pull");
     }
 }
