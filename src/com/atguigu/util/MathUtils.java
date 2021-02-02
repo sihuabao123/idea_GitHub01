8a23b5219ab0b199981fb68e5cc92353a5ae799f
sihuabao123 <78328077+sihuabao123@users.noreply.github.com>
2021-02-02 23:10:32 +0800

    GitHub_修改idea_GitHub01
    GitHub_修改idea_GitHub01

--- src/com/atguigu/util/MathUtils.java
+++ src/com/atguigu/util/MathUtils.java
@@ -4,5 +4,6 @@ public class MathUtils {
     public static void main(String[] args) {
         System.out.println("11111master");
         System.out.println("11111dev");
+        System.out.println("GitHub1111");
     }
 }
